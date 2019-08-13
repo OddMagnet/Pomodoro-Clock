df2a6de4fca177ae773555bbe22ac5d663627bee
Michael Brünen <mb156208@fh-muenster.de>
2019-08-13 18:43:04 +0200

    Updates

---
+++ precache-manifest.242694d850f52fc055bd5035498f5c2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80c843e656776f269ebc2749b4025d3d",
+    "url": "/Pomodoro-Clock/index.html"
+  },
+  {
+    "revision": "8b5c02eff234b16e80f9",
+    "url": "/Pomodoro-Clock/static/css/main.dcd89852.chunk.css"
+  },
+  {
+    "revision": "eed5872a7ad4b62e0fe0",
+    "url": "/Pomodoro-Clock/static/js/2.7d4ff228.chunk.js"
+  },
+  {
+    "revision": "8b5c02eff234b16e80f9",
+    "url": "/Pomodoro-Clock/static/js/main.f48fcaa1.chunk.js"
+  },
+  {
+    "revision": "37bb79882c781a8c14fe",
+    "url": "/Pomodoro-Clock/static/js/runtime~main.d020cda9.js"
+  },
+  {
+    "revision": "0acf9f43025a8a97bad15026b2773793",
+    "url": "/Pomodoro-Clock/static/media/beep-06.0acf9f43.wav"
+  }
+]);
