64b1b9d6f5acff77c6bdad4a3cb06a1f5bb0180d
Michael Brünen <mb156208@fh-muenster.de>
2019-08-13 15:13:21 +0200

    Add state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      breakLength: 5,
+      sessionLength: 25,
+      timerState: 'isStopped',  // 'isRunning'
+      timerType: 'Session',     // 'Break'
+      timer: 1500,              // timer in seconds, 25*60 = 1500
+      intervalID: '',
+      alarmColor: { color: 'white' }  // default of white, setting to red when < 60s
+    }
+  }
 
   render() {
     return (
-      <h1>Pomodoro Clock</h1>
+      <div>
+        <h1>Pomodoro Clock</h1>
+      </div>
     );
   }
 }
